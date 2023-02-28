5daeaee7bfe071b0662be6769c42d78347f303a3
Elgun Gafarzade <elgungafarzade@Elgun-MacBook-Air.local>
2023-02-28 11:48:37 +0400

    27 february hometask

---
+++ hometask/Program.cs
@@ -0,0 +1,36 @@
+﻿#region task1
+//int[] arr = { 4, 7, 8, 9, 23, 66 };
+//int num = 9;
+//Axtar(arr, num);
+
+//void Axtar(int[] arr,int num)
+//{
+
+//    foreach (var i in arr)
+//    {
+//        if(i==num)
+//        {
+//            Console.WriteLine("var");
+//            break;
+//        }
+//    }
+//}
+#endregion
+
+#region task2
+
+//int Enboyuk(params int[] arr);
+
+//{
+//    int Max = 0;
+//    foreach (int number in arr)
+//    {
+//        if (number>Max)
+//        {
+//            Max = number;
+//        }
+//    }
+
+//    return Max;
+//}
+#endregion
